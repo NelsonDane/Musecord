acfa66f20a2ee9d9ff4aaa2ea0a149929749fa9e
Nelson Dane <danenb@clarkson.edu>
2022-06-10 13:32:23 -0500

    Use double quotes to prevent globbing and word spitting

--- LibreScore.sh
+++ LibreScore.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npx --yes dl-librescore@latest -i $1 -t $2 -o $3 #URL, type, path. --yes to bypass install confirmation
+npx --yes dl-librescore@latest -i "$1" -t "$2" -o "$3" #URL, type, path. --yes to bypass install confirmation
